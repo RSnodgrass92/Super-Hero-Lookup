5622b3f06e0c986c7c69998ecd06a6cfdbeb8985
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-09-07 13:21:06 -0600

    inital setup, basic depencies added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <div className="row">
+        <div className="col-6">test1</div>
+        <div className="col-6">test2</div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
